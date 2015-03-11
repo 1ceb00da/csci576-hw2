cebad6bebbe1d17d79e3a2f3867485865496db4c
Aditya Dhulipala <aditya.d@hotmail.com>
2015-03-10 23:23:17 -0700

    convergance logic not working correctly yet

--- csci576-hw2/src/KMeans.java
+++ csci576-hw2/src/KMeans.java
@@ -7,6 +7,26 @@ import edu.usc.adhulipa.DataStructs.Vector;
 
 public class KMeans {
 
+	public static int meanDiff(List<Vector<Integer, Integer>> oldMeans,
+			List<Vector<Integer, Integer>> newMeans) {
+
+		System.out.println("\n in mean diff fn");
+		System.out.println(oldMeans);
+		System.out.println(newMeans);
+		
+		Vector<Integer, Integer> oldMean, newMean;
+		List<Vector<Integer, Integer>> diffs = new ArrayList<Vector<Integer, Integer>>();
+		
+		for (int i = 0; i < newMeans.size(); i++) {
+			oldMean = oldMeans.get(i);
+			newMean = newMeans.get(i);
+			diffs.add(Vector.difference(oldMean, newMean));
+		}
+		System.out.println(diffs + " are diffs");
+		
+		
+		return 0;
+	}
 
 	public static int findClosestMean(Vector<Integer, Integer> vec,
 			List<Vector<Integer, Integer>> means) {
@@ -35,10 +55,10 @@ public class KMeans {
 		
 		
 		double count;
-		
+		List<Vector<Integer, Integer>> newMeans = new ArrayList<Vector<Integer, Integer>>(means);
+
 
 		for (int k = 0 ; k < means.size(); k++) {
-			Byte Zero = new Integer(0).byteValue();
 			count = 0;
 			
 			int sumLeft = 0;
@@ -61,10 +81,10 @@ public class KMeans {
 			
 			Vector<Integer, Integer> newMean = Vector.of(new Double(sumLeft/count).intValue(), new Double(sumRight/count).intValue());
 			
-			means.set(k, newMean);
+			newMeans.set(k, newMean);
 		}
 		
-		return means;		
+		return newMeans;		
 	}
 
 	private static double distance(Vector<Integer, Integer> mean, Vector<Integer, Integer> vec) {
